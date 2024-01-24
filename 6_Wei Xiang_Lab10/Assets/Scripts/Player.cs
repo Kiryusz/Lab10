78d3b26f3c52c8f13ecccdbc539424556ee115c7
Kiryusz <ongweixang27@gmail.com>
2024-01-24 10:20:08 +0800

    Updated game

---
+++ 6_Wei Xiang_Lab10/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private float YMin = -3.9f;
+    private float YMax = 3.9f;
+    public GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+        transform.position = new Vector3(-5.81f, Mathf.Clamp(transform.position.y, YMin, YMax), -0.04771193f);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag=="Obstacle")
+        {
+            gm.Loser();
+        }
+        if (collision.gameObject.tag=="Score")
+        {
+            gm.ScoreIncrement();
+        }
+    }
+}
---
+++ 6_Wei Xiang_Lab10/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject [] SpawnObject;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        int random = Random.Range(0, 2);
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObject[random], transform.position, transform.rotation);
+    }
+}
